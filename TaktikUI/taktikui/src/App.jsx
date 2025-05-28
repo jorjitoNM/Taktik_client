a78fe28f7ff108d241459f30cb7cf11297f05b01
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-05-28 10:49:44 +0200

    He añadido las rutas asociadas a los componente de login y register.

--- TaktikUI/taktikui/src/App.jsx
+++ TaktikUI/taktikui/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import AnimatedRoutes from './AnimatedRoutes';
-import PantallaInicio from "./components/PantallaInicio";
+import LoginScreen from "./components/LoginScreen";
 
 function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<PantallaInicio />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<  RegisterScreen />} />
         </Routes>
     </Router>
   );
