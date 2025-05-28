d9a1ca7f1a2df25dd950fd72a5517cd6b09d7bc5
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-05-28 14:10:21 +0200

    He corregido el import del root componente App.

--- TaktikUI/taktikui/src/EntryPoint/main.jsx
+++ TaktikUI/taktikui/src/EntryPoint/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from '../components/RootComponent/App.jsx'
+import App from '../components/RootComponent/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
