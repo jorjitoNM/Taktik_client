3ca0e6916564d91974d1c70c74852058a5a5bae0
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-05-29 14:07:53 +0200

    He eliminado el archivo auxiliar .gitkeep y el archivo CSS principal.

--- TaktikUI/taktikui/src/entryPoint/main.jsx
+++ TaktikUI/taktikui/src/entryPoint/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from '../components/RootComponent/App'
 
 createRoot(document.getElementById('root')).render(
