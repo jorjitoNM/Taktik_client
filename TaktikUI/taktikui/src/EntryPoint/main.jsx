5d56d643f5eb4650456193239a0f51e134278467
Antonio Mateos Uriarte <antoniomateos2233@gmail.com>
2025-05-29 12:14:17 +0200

    He cambiado los nombres de las rutas del componente root App y del entry point.

--- TaktikUI/taktikui/src/EntryPoint/main.jsx
+++ TaktikUI/taktikui/src/EntryPoint/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '../AppComponent/App.jsx'
+import App from '../components/RootComponent/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
