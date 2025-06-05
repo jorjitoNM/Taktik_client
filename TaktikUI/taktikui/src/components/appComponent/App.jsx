fe490b6a7e97f3bb98fbbd52e8f62fc5245b22a2
DanielEstebanM <daniel.estebanmiranda@outlook.com>
2025-06-05 12:03:57 +0200

    Cambios

--- TaktikUI/taktikui/src/components/appComponent/App.jsx
+++ TaktikUI/taktikui/src/components/appComponent/App.jsx
@@ -2,13 +2,6 @@ import './App.css'
 import HomeScreen from '../homeComponent/HomeScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-export default App;
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   return (
     <Router>
