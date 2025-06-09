b16270bd325d39e898651c6b6d79b8a8c94f51e8
DanielEstebanM <daniel.estebanmiranda@outlook.com>
2025-06-09 08:53:41 +0200

    Cambios finales

--- TaktikUI/taktikui/src/components/appComponent/App.jsx
+++ TaktikUI/taktikui/src/components/appComponent/App.jsx
@@ -1,18 +1,18 @@
 
-  import './App.css';
+import './App.css';
 import ProjectScreen from '../projectComponent/ProjectScreen';
-  import HomeScreen from '../homeComponent/HomeScreen';
+import HomeScreen from '../homeComponent/HomeScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        <Routes>
-          {/* <Route path="/login" element={<LoginScreen />} /> */}
-          {/* <Route path="/register" element={<RegisterScreen />} /> */}
-<Route path="/project/:projectId" element={<ProjectScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+      <Routes>
+        {/* <Route path="/login" element={<LoginScreen />} /> */}
+        {/* <Route path="/register" element={<RegisterScreen />} /> */}
+        <Route path="/projects/:projectId" element={<ProjectScreen />} />
+        <Route path="/" element={<HomeScreen />} />
+      </Routes>
     </Router>
   );
 }
